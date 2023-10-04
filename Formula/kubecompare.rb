1ac90868c6371b51f4ac881f0761943017c45495
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2023-10-04 17:55:09 +0200

    0.1.4

--- Formula/kubecompare.rb
+++ Formula/kubecompare.rb
@@ -1,13 +1,16 @@
 class Kubecompare < Formula
     desc "A tool to compare Kubernetes rollout histories"
-    url "https://github.com/wozniakpl/kubecompare/archive/v0.1.3.tar.gz"
-    sha256 "ad03fe1002515ee2d4347f94bf59ffa30f86f6f6e8500e666148a9e105fb7bae"
+    url "https://github.com/wozniakpl/kubecompare/archive/v0.1.4.tar.gz"
+    sha256 "36b57c7732ba3068d737d861779ac9a150999e91ad8bfe87f610c56ccb04930f"
     license "MIT"
   
     depends_on "go" => :build
   
     def install
       system "go", "build", *std_go_args, "-o", bin/"kubecompare"
+
+      bash_completion.install "completions/kubecompare_bash_completion.sh" => "kubecompare"
+      zsh_completion.install "completions/_kubecompare"
     end
   
     test do
